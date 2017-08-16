2093be394253bf88a3f29ca0193b8a273f3b4dd5
Philippe Damen <philippe@thinktomorrow.be>
2017-08-16 12:05:18 +0200

    removed htmlLawed import from utils as its loaded in the service provider

--- src/Manager/utils/helpers.php
+++ src/Manager/utils/helpers.php
@@ -35,8 +35,6 @@ if(!function_exists('squantoCleanupHTML'))
 {
     function squantoCleanupHTML( $value, $whitelist = null )
     {
-        require_once __DIR__ . '/vendors/htmlLawed.php';
-
         if(is_null($whitelist))
         {
             $whitelist = '<code><span><div><label><a><br><p><b><i><del><strike><u><img><video><audio><iframe><object><embed><param><blockquote><mark><cite><small><ul><ol><li><hr><dl><dt><dd><sup><sub><big><pre><code><figure><figcaption><strong><em><table><tr><td><th><tbody><thead><tfoot><h1><h2><h3><h4><h5><h6>';
